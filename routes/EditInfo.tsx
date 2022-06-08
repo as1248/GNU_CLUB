744718e56a18608b50589c95f78da84667188e36
smoothyking <gogo5896544@gmail.com>
2022-06-08 18:19:00 +0900

    정보수정페이지 오류 수정

--- routes/EditInfo.tsx
+++ routes/EditInfo.tsx
@@ -2,17 +2,10 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import React, { useState } from 'react';
 import {View,Text,TextInput,Image, StyleSheet, TouchableOpacity} from 'react-native';
 import styled from 'styled-components/native';
-import * as ImagePicker from 'expo-image-picker';
 
 const Main = styled.View`
     display: flex;
     align-items: center;
-`;
-
-const Images = styled.View`
-    width: 40%;
-    margin-left: 10%;
-    margin-bottom: 10%;
 `;
 
 const ProfileImg = styled.View`
@@ -33,35 +26,6 @@ const EditInfo = () => {
     const [pw, setpw] = useState("");
     const [pwn, setPwn] = useState("");
     const [pwnc, setPwnc] = useState("");
-    const [image, setImage] = useState();
-
-    const pickImage = async () => {
-        let result:any = await ImagePicer.launchImageLibraryAsync({
-          mediaTypes: ImagePicker.MediaTypeOptions.All,
-          allowsEditing: true,
-          aspect: [1,1],
-          quality: 1,
-        });
-        setImage(result.uri);
-        }
-        const callApi = async() => {
-            try{
-                const formData = new FormData();
-                formData.append('image',null);
-                formData.append('dto', {'string': JSON.stringify({title, content}), type: 'application/json'});
-                await axios({
-                    method: 'post',
-                    url: `http://15.165.169.129/api/member/{memberPK}/profile_image`,
-                    data: formData,
-                    headers: {
-                        'Content-Type': 'multipart/form-data',
-                        'Accept': '*/*'
-                    }
-                });
-                }catch(error){
-                console.log(error.response.data);
-            }
-        }
 
     const requestpofileimg = async () => {      
     }
@@ -108,15 +72,8 @@ const EditInfo = () => {
     
     return (
         <Main>
-            <View style={{width: "100%",borderColor: "black", padding: "14%", marginTop: "20%", marginBottom: "25%", display: "flex", flexDirection: "row", justifyContent: "space-between", alignItems: "center"}}>
-            <Images>
-                <Image source={{ uri: image }} style={{ width: 100, height: 100 }} />
-             
-                <TouchableOpacity style={styles.profileImgBtn} onPress={pickImage()}>
-                    <Image style={{width: 100, height: 100}} source={require('../assets/icon.png')} />
-                    </TouchableOpacity>
-
-            </Images>
+            <View style={{width: "100%", padding: "10%", marginTop: "20%", marginBottom: "25%", display: "flex", flexDirection: "row", justifyContent: "space-between", alignItems: "center"}}>
+                <Image style={{width: 100, height: 100}} source={require('../assets/icon.png')} />
                 <TouchableOpacity style={styles.profileImgBtn} onPress={requestpofileimg}>
                     <Text style={{fontSize: 20, color: "white"}}>프로필사진수정</Text>
                 </TouchableOpacity>
@@ -126,7 +83,7 @@ const EditInfo = () => {
                 <TextInput style={{marginBottom: "8%"}} placeholder="새비밀번호" onChangeText={(text) => setPwn(text)} /> 
                 <TextInput style={{marginBottom: "8%"}} placeholder="새비밀번호확인" onChangeText={(text) => setPwnc(text)} /> 
                 <TouchableOpacity style={styles.passwordBtn} onPress={requestAlterpw}>
-            <Text style={{fontSize: 20, color: "white"}}>비밀번호변경d</Text>
+            <Text style={{fontSize: 20, color: "white"}}>비밀번호변경</Text>
         </TouchableOpacity>
             </ChangeInfo> 
         </Main>
@@ -143,7 +100,7 @@ const styles = StyleSheet.create({
         borderRadius: 10, backgroundColor: "skyblue", width: 150, height: 50, justifyContent: "center", alignItems: "center" 
     },
     passwordBtn: {
-        borderRadius: 10, backgroundColor: "skyblue", width: 290, height: 50, justifyContent: "center", alignItems: "center" 
+        borderRadius: 10, backgroundColor: "skyblue", width: 250, height: 50, justifyContent: "center", alignItems: "center" 
     },
     flowDirection: {
         flexDirection: "row"
